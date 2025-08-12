ee114f3228bf595eb0cc22c2620b586dd997345a
K-JunHo <kalmia6863@gmail.com>
2025-08-12 16:30:05 +0900

    Add date & time.

---
+++ js/clock.js
@@ -0,0 +1,30 @@
+const monthNames = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
+const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const clockDate = document.getElementById("clock__date");
+const clockTime = document.getElementById("clock__time");
+
+function getClock() {
+	const date = new Date();
+	clockDate.innerText = `${dayNames[date.getDay()]}, ${
+		monthNames[date.getMonth()]
+	} ${date.getDate()}, ${date.getFullYear()}`;
+	clockTime.innerText = `${String(date.getHours()).padStart(2, "0")}:${String(
+		date.getMinutes()
+	).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}`;
+}
+
+getClock();
+setInterval(getClock, 1000);

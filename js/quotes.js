d84eedc8d822df25d2c0643ffa9d8dc8f9bd80a5
K-JunHo <kalmia6863@gmail.com>
2025-08-13 17:53:02 +0900

    Add styles.

--- js/quotes.js
+++ js/quotes.js
@@ -101,4 +101,4 @@ const author = document.querySelector("#quote span:last-child");
 const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = randomQuote.quote;
-author.innerText = randomQuote.author;
+author.innerText = `- ${randomQuote.author} -`;
